f881068160a00e29f68e43c5194337780c3866cf
Arlan Maximov <maxarlan974@gmail.com>
2026-01-25 20:28:03 +0500

    Add Practice1 Python basics examples

---
+++ strings.py
@@ -0,0 +1,25 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+for x in "banana":
+  print(x)
+
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+b = "Hello, World!"
+print(b[2:5])
+print(b.replace("H", "J"))
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = "Hello, welcome to my world."
+x = txt.find("welcome")
+print(x)
